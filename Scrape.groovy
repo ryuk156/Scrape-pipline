6c01ef240d36a31366e5f5107f4dc604446ab97e
Yash Patel <49101492+ryuk156@users.noreply.github.com>
2021-05-27 21:26:44 +0530

    Update Scrape.groovy

--- Scrape.groovy
+++ Scrape.groovy
@@ -64,14 +64,3 @@ def exec() {
 	}
 }
 
-def push() {
-	withCredentials([usernamePassword(credentialsId: 'GooeyHub', passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]) {
-    	sh('git config --global user.email "terasology@gmail.com"')
-    	sh('git config --global user.name "GooeyHub"')
-    	sh('git add .')
-    	sh('git commit -m \"Updated Index Data\"')
-    	sh('git push https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/GooeyTests/TempIndex.git --all')
-	}
-}
-
-return this
